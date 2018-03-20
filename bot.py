568346372e09683e41edebcafa29819383787f5c
nyashabryan <nyashabryan23@gmail.com>
2018-03-20 13:02:49 +0000

    Added the get methods to get the consumer secrets from the keys file

--- bot.py
+++ bot.py
@@ -6,15 +6,39 @@
 
 import tweepy
 
+keys_file = "keys"
+
+def get_access_token_key():
+    return get("access_token_key")
+
+def get_access_token():
+    return get("access_token")
+
+def get_consumer_secret():
+    return get("consumer_secret")
+
+def get_consumer_key():
+    return get("consumer_key")
+
+def get(String::req):
+    global keys_file
+    with open(keys_file, "r:") as kfile:
+	lines = kfile.readlines()
+        for line in lines:
+            if line.split(": ")[0] = req:
+                return line.split(": ")[1][:-1]
+    else:
+        return "None"
+	
 
 def main():
+    
     auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 
-    try:
-        redirect_url = auth.get_authorization_url()
-    except: 
-        print('Error! Failed to get request token.')
+    auth.set_access_token(access_token_key, access_token_secret)
+    
+    api =  tweepy.API(auth)
+    
+    api.update_status("I am in this" )
 
-
-    session.set('request_token', auth.request_token)
-
+main()
