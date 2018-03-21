7542f5df555e8cd4d8cd03346e86c3feb7b1cfd3
nyashabryan <nyashabryan23@gmail.com>
2018-03-21 12:52:22 +0000

    Various changes.

---
+++ test.py
@@ -0,0 +1,19 @@
+import bot.py
+
+def test_get():
+
+    #Test 1:
+    print (get_access_token())
+
+    #Test 2:
+    print (get_access_token_key())
+
+    #Test 3:
+    print (get_consumer_key())
+
+    #Test 4: 
+    print (get_consumer_secret())
+
+
+if __name__ = "__main__":
+    test_get()
